9b82fb16376d2dac9c2b84a60a3494873280ad07
Javier Armero <Javalba90@gmail.com>
2018-02-05 00:01:02 +0100

    feature module

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 
 @NgModule({
@@ -12,7 +14,9 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PassengerDashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
