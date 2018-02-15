81e30c543e112feadae81b0087253d623a2763a2
Javier Armero <Javalba90@gmail.com>
2018-02-15 23:19:42 +0100

    Services, Http and Observables

--- angular-basics/src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ angular-basics/src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,6 +1,7 @@
-import { PassengerDashboardService } from './services/passenger-dashboard.service';
 import { CommonModule } from '@angular/common';
 import { NgModule } from "@angular/core";
+import { HttpModule } from '@angular/http';
+
 
 //Container
 import { PassengerDashboardComponent } from './container/passenger-dashboard/passenger-dashboard.component';
@@ -11,10 +12,14 @@ import { PassengerCountComponent } from './components/passenger-count/passenger-
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 import { PassengerInfoComponent } from './components/passenger-info/passenger-info.component';
 
+//Services
+import { PassengerDashboardService } from './services/passenger-dashboard.service';
+
 @NgModule({
   declarations:[PassengerDashboardComponent, PassengerActionsComponent, PassengerCountComponent, PassengerDetailComponent, PassengerInfoComponent],
   imports:[
-    CommonModule
+    CommonModule,
+    HttpModule
   ],
   exports: [
     PassengerDashboardComponent //The rest of components are referred internally.
--- angular-basics/src/app/passenger-dashboard/services/passenger-dashboard.service.ts
+++ angular-basics/src/app/passenger-dashboard/services/passenger-dashboard.service.ts
@@ -1,49 +1,35 @@
+import { Passenger } from './../models/passenger.interface';
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
-import { Passenger } from '../models/passenger.interface';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
+const PASSENGER_API: string = `${environment.backend}/passengers`;
 
 @Injectable()
 export class PassengerDashboardService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  getPassengers(): Passenger[] {
-
-    return [{
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1510742000000,
-      children: [{ name: 'Ted', age: 3 }],
-      nationality: null
-    }, {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      children: null,
-      nationality: 'france.svg'
-    }, {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkInDate: 1510742000000,
-      children: [{ name: 'Chloe', age: 7 }, { name: 'Emma', age: 5 }],
-      nationality: 'united-states-of-america.svg'
-    }, {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkInDate: 1510742000000,
-      children: null,
-      nationality: null
-    }, {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-      children: null,
-      nationality: 'united-kingdom.svg'
-    }
-    ];
-    
+  getPassengers(): Observable<Passenger[]>{
+    return this.http
+    .get(PASSENGER_API)
+    .map((response: Response) => response.json());
   }
+
+  updatePassenger(passenger: Passenger): Observable<Passenger[]>{
+    return this.http
+    .put(`${PASSENGER_API}/${passenger.id}`, passenger)
+    .map((response: Response) => response.json());
+  }
+
+  removePassenger(passenger: Passenger): Observable<Passenger[]>{
+    return this.http
+    .delete(`${PASSENGER_API}/${passenger.id}`)
+    .map((response: Response) => response.json());
+  }
+
 
 }
